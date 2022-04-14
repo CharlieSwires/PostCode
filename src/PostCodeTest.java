26e72d77fbd4c2c483897cf5e63d98c7f8134ef6
CharlieSwires <charlieswires@gmail.com>
2022-04-14 20:59:05 +0100

    seperate tests

--- src/PostCodeTest.java
+++ src/PostCodeTest.java
@@ -9,15 +9,35 @@ class PostCodeTest {
     }
 
     @Test
-    void test() {
+    void test1() {
         Assertions.assertTrue(PostCode.valid("M21 8HR"));
+    }
+    @Test
+    void test2() {
         Assertions.assertTrue(PostCode.valid("HD3 6FD"));
+    }
+    @Test
+    void test3() {
         Assertions.assertTrue(PostCode.valid("BD23 2QR"));
+    }
+    @Test
+    void test4() {
         Assertions.assertTrue(PostCode.valid("B2 2GT"));
-
+    }
+    @Test
+    void test5() {
         Assertions.assertFalse(PostCode.valid("B124 5BF"));
+    }
+    @Test
+    void test6() {
         Assertions.assertFalse(PostCode.valid("LS21 21WS"));
+    }
+    @Test
+    void test7() {
         Assertions.assertFalse(PostCode.valid("265 7RJ"));
+    }
+    @Test
+    void test8() {
         Assertions.assertFalse(PostCode.valid("L23 45GF"));
     }
 
